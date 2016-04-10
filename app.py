9ad0629f82a3bf1ba6a84592132002a16cafab79
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-04-10 09:24:32 -0700

    removing some comments

--- app.py
+++ app.py
@@ -14,15 +14,8 @@ import psycopg2
 
 @app.route("/")
 def index():
-  # print db
-  # events = db.execute('select duration from parkingevents')
-  # print events
-  # garages = db.execute('select * from garages')
-  # eventList = events.fetchAll()
-  # garageList = garages.fetchAll()
   parkingevents = ParkingEvent.query.all()
   # garagesList = Garage.query.all()
-  # print parkingevents[0]
   return render_template("homepage.html", events=parkingevents)
 
 if __name__ == "__main__":
