acfb01a8072ba9ef1bbc99b1b29193a2f5385605
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-04-10 10:44:40 -0700

    trying to get request method in controller

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 from model import connect_to_db, ParkingEvent
+import os
+import requests
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
@@ -13,11 +15,20 @@ import psycopg2
 #     print "I am unable to connect to the database"
 
 
+
 @app.route("/")
 def index():
   parkingevents = ParkingEvent.query.all()
   # garagesList = Garage.query.all()
   return render_template("homepage.html", events=parkingevents)
+
+@app.route('/automatic_api')
+def get_automatic_json():
+  access_client_id = os.envrion['AUTOMATIC_CLIENT_ID']
+  access_secret = os.environ['AUTOMATIC_SECRET']
+  print access_client_id
+  print access_secret
+  return jsonify({json:"hello you got the data"})
 
 
 if __name__ == "__main__":
