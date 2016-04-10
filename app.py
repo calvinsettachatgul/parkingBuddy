1781c45b9668c11d912be3ab1f302ecf3cb634e0
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-04-10 10:26:57 -0700

    trying to get js

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 from model import connect_to_db, ParkingEvent
+
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
@@ -17,6 +18,7 @@ def index():
   parkingevents = ParkingEvent.query.all()
   # garagesList = Garage.query.all()
   return render_template("homepage.html", events=parkingevents)
+
 
 if __name__ == "__main__":
   app.debug = False
